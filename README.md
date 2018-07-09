c4ffaea532818cf7c58618001496ded6ff7e850f
rogerp67 <roger@cambit.nl>
2018-07-09 19:19:46 +0200

    Added speaker support for controlling the homebridge/Yamaha device

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-"# homebridge-yamaha_mc" 
-Add this to the config.json, the last 3 items adjusted to your own situation:
+﻿"# homebridge-yamaha_mc" 
+Based on the Yamaha Extended Control API Spec (https://github.com/samvdb/php-musiccast-api/blob/master/yxc-api-spec-advanced.pdf)
+With this homebridge module you you can switch on and switch off your Yamaha Networked devices, in my case MusicCast devices.
+Since version 1.1 you can also use the Speaker functionality, beside the Switch functionality. Unfortunately HomeKit does not yet support this speaker accessory, it only sees the switch.
+An app like Legato Eve does support it. You will see 2 devices on for your device: a switch and a speaker. Hopefully the next homekit app will too.
+ 
+ 
+Add this to the config.json, for each device you want to add. Just adjust the name and the IP address for each accessory. 
+The zone 'main' in general should work, not to be confused with room names.
+For my Yamaha RN-602 the maximum volume setting is 161, for the W-030 it is 60. 
+Check the max volume setting by using this url in your homenetwork: http://192.168.1.220/YamahaExtendedControl/v1/system/getFeatures (with your own IP of course)
+Find the value at  "range step", volume, max in the JSON string result.
 
 "accessories": [
         {
           "accessory": "YamahaMC",
           "name": "Yamaha RN-602",
           "host": "192.168.1.210",
-          "zone": "main"
+          "zone": "main",
+		  "maxvol":161
         }   
     ]
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var Service, Characteristic;
+
 const request = require('request');
 const url = require('url');
  
-var yamaha, zone;
+var yamaha;
 
-var Service, Characteristic;
+
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
@@ -13,34 +15,45 @@ module.exports = function(homebridge) {
 }
 
 function Yamaha_mcAccessory(log, config) {
+  this.currentState = false;
   this.log = log;
   this.name = config["name"];
   this.host = config["host"];
   this.zone = config["zone"];
+  this.maxVol = config["maxvol"];
 }
 
 Yamaha_mcAccessory.prototype = {
   getServices: function () {
     let informationService = new Service.AccessoryInformation();
     informationService
-      .setCharacteristic(Characteristic.Manufacturer, "Yamaha")
-      .setCharacteristic(Characteristic.Model, "RN-602")
-      .setCharacteristic(Characteristic.SerialNumber, "551663");
+      .setCharacteristic(Characteristic.Manufacturer, "Cambit")
+      .setCharacteristic(Characteristic.Model, "Yamaha MC")
+      .setCharacteristic(Characteristic.SerialNumber, "6710160330");
  
-    let switchService = new Service.Switch("Yamaha MC");
+    let switchService = new Service.Switch("Receiver");
     switchService
       .getCharacteristic(Characteristic.On)
         .on('get', this.getSwitchOnCharacteristic.bind(this))
         .on('set', this.setSwitchOnCharacteristic.bind(this));
- 
+
+    let speakerService = new Service.Speaker("Receiver");
+    speakerService
+      .getCharacteristic(Characteristic.Mute)
+        .on('get', this.getSpeakerMutedCharacteristic.bind(this))
+        .on('set', this.setSpeakerMutedCharacteristic.bind(this));
+
+    speakerService
+      .getCharacteristic(Characteristic.Volume)
+        .on('get', this.getSpeakerVolumeCharacteristic.bind(this))
+        .on('set', this.setSpeakerVolumeCharacteristic.bind(this));
+
     this.informationService = informationService;
     this.switchService = switchService;
-    return [informationService, switchService];
-  }
-}; 
-
-Yamaha_mcAccessory.prototype = {
- 
+	this.speakerService = speakerService;
+    return [informationService, switchService, speakerService];
+  },
+  
   getSwitchOnCharacteristic: function (next) {
     const me = this;
     request({
@@ -49,31 +62,121 @@ Yamaha_mcAccessory.prototype = {
             headers: {
                 'X-AppName': 'MusicCast/1.0',
                 'X-AppPort': '41100',
+			},
     }, 
     function (error, response, body) {
       if (error) {
-        me.log('STATUS: ' + response.statusCode);
+        //me.log('HTTP get error ');
         me.log(error.message);
         return next(error);
       }
-      return next(null, (body.power=='on'));
+	  att=JSON.parse(body);
+	  me.log('HTTP GetStatus result:' + (att.power=='on' ? "On" : "Off"));
+      return next(null, (att.power=='on'));
     });
   },
    
   setSwitchOnCharacteristic: function (on, next) {
-    const me = this;
+    var url='http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/setPower?power=' + (on ? 'on' : 'standby');
+	const me = this;
     request({
-      url: 'http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/setPower?power=' + (on ? 'on' : 'standby') ,
-      body: {'targetState': on},
-      method: 'POST',
-      headers: {'Content-type': 'application/json'}
+      url: url  ,
+      method: 'GET',
+      body: ""
     },
     function (error, response) {
       if (error) {
-        me.log('STATUS: ' + response.statusCode);
+        //me.log('error with HTTP url='+url);
         me.log(error.message);
         return next(error);
       }
+	  //me.log('HTTP setPower succeeded with url:' + url);
+      return next();
+    });
+  },
+  
+  // speaker characteristics
+  
+  getSpeakerMutedCharacteristic: function (next) {
+    const me = this;
+    request({
+        method: 'GET',
+            url: 'http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/getStatus',
+            headers: {
+                'X-AppName': 'MusicCast/1.0',
+                'X-AppPort': '41100',
+			},
+    }, 
+    function (error, response, body) {
+      if (error) {
+        //me.log('HTTP get error ');
+        me.log(error.message);
+        return next(error);
+      }
+	  att=JSON.parse(body);
+	  me.log('HTTP GetStatus result:' + (att.mute=='true' ? "On" : "Off"));
+      return next(null, (att.mute=='true'));
+    });
+  },
+   
+  setSpeakerMutedCharacteristic: function (muted, next) {
+    var url='http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/setMute?enable=' + (muted ? 'true' : 'false');
+	const me = this;
+    request({
+      url: url  ,
+      method: 'GET',
+      body: ""
+    },
+    function (error, response) {
+      if (error) {
+        //me.log('error with HTTP url='+url);
+        me.log(error.message);
+        return next(error);
+      }
+	  //me.log('HTTP setMuted succeeded with url:' + url);
+      return next();
+    });
+  },
+  
+  getSpeakerVolumeCharacteristic: function (next) {
+    const me = this;
+	var res;
+    request({
+        method: 'GET',
+            url: 'http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/getStatus',
+            headers: {
+                'X-AppName': 'MusicCast/1.0',
+                'X-AppPort': '41100',
+			},
+    }, 
+    function (error, response, body) {
+      if (error) {
+        //me.log('HTTP get error ');
+        me.log(error.message);
+        return next(error);
+      }
+	  att=JSON.parse(body);
+	  res = Math.floor(att.volume / this.maxVol * 100);
+	  me.log('HTTP GetStatus result:' + res);
+      return next(null, res);
+    });
+  },
+   
+  setSpeakerVolumeCharacteristic: function (volume, next) {
+    var url='http://' + this.host + '/YamahaExtendedControl/v1/' + this.zone + '/setVolume?volume=' + Math.floor(volume/100 * this.maxVol);
+	const me = this;
+    request({
+      url: url  ,
+      method: 'GET',
+      body: ""
+    },
+    function (error, response) {
+      if (error) {
+        //me.log('error with HTTP url='+url);
+        me.log(error.message);
+        return next(error);
+      }
+	  //me.log('HTTP setVolume succeeded with url:' + url);
       return next();
     });
   }
